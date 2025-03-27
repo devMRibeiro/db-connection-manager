4befc55280fc5f15964176db8a3a35b5d3521779
devMRibeiro <michael.douglas2020@hotmail.com>
2025-03-26 23:34:34 -0300

    Adiciona documentação

--- src/main/java/com/db/utility/ConnFactory.java
+++ src/main/java/com/db/utility/ConnFactory.java
@@ -4,19 +4,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
+/**
+ * A classe {@code ConnFactory} é responsável por gerenciar a criação e 
+ * o fechamento de conexões com o banco de dados.
+ * Ela fornece métodos estáticos para abrir e fechar conexões,
+ * garantindo que os recursos sejam manipulados adequadamente.
+ * 
+ * <p>Este utilitário usa um arquivo de configuração de propriedades {@code application.properties}
+ * para obter os dados necessários para a conexão com o banco, como a URL, o usuário e a senha.</p>
+ * 
+ * <p>O método {@code open()} tenta abrir uma conexão com o banco de dados com base nas
+ * configurações fornecidas no arquivo {@code application.properties}.
+ * Se o arquivo não for encontrado ou estiver mal-formatado, será exibida uma mensagem de erro.</p>
+ * 
+ * <p>O método {@code close()} é utilizado para fechar os recursos {@code AutoCloseable},
+ * como conexões, ResultSets e Statements, garantindo que todos os recursos sejam corretamente liberados.</p>
+ * 
+ * @author Michael D. Ribeiro
+ */
 public class ConnFactory {
+
+	/**
+   * Estabelece uma conexão com o banco de dados utilizando as propriedades fornecidas
+   * no arquivo {@code application.properties}.
+   * 
+   * <p>Este método carrega as propriedades de configuração do banco de dados
+   * a partir do arquivo de propriedades e tenta estabelecer uma conexão utilizando o {@code DriverManager}.
+   * Se o arquivo de propriedades não for encontrado ou estiver mal-formatado, será retornado {@code null}.</p>
+   *
+   * @return Uma instância de {@link Connection} representando a conexão com o banco de dados,
+   * 					ou {@code null} se ocorrer um erro ao tentar estabelecer a conexão.
+   */
 	public static Connection open() {
-
 		Properties props = new Properties();
-
 		try {
 
 			props.load(ConnFactory.class.getClassLoader().getResourceAsStream("application.properties"));
 
-			if (props == null || props.isEmpty()) {
-				System.out.println("application.properties não encontrado ou arquivo mal-formatado.");
-				return null;
-			}
+			if (props == null || props.isEmpty())
+				throw new IllegalArgumentException("application.properties não encontrado ou arquivo mal-formatado.");
 
 			return DriverManager.getConnection(
 					props.getProperty("DB_URL"),
@@ -24,18 +50,24 @@ public class ConnFactory {
 					props.getProperty("DB_PASS"));
 
 		} catch (Exception e) {
-			System.out.println("Erro ao tentar estabelecer uma conexão.");
-			e.printStackTrace();
+			throw new RuntimeException("Erro ao tentar estabelecer uma conexão.", e);
 		}
-		return null;
 	}
 
+	/**
+   * Libera os recursos {@code AutoCloseable} passados como parâmetros.
+   * 
+   * <p>Este método percorre todos os recursos passados no array e tenta fechá-los, começando pelo último recurso. 
+   * Caso um recurso não possa ser fechado corretamente, uma mensagem de erro será exibida. 
+   * É importante garantir que todos os recursos sejam fechados para evitar vazamentos de memória ou conexões.</p>
+   * 
+   * @param resources Array de recursos {@code AutoCloseable} a serem fechados. 
+   *         Ao menos um recurso deve ser fornecido, caso contrário, uma mensagem de erro será exibida.
+   */
 	public static void close(AutoCloseable ... resources) {
 
-		if (resources.length == 0) {
-			System.out.println("Deve conter pelo menos 1 recurso AutoCloseable");
-			return;
-		}
+		if (resources.length == 0)
+			throw new IllegalArgumentException("Deve conter pelo menos 1 recurso AutoCloseable");
 
 		for (int i = resources.length - 1; i >= 0; i--) {
 			AutoCloseable resource = resources[i];
@@ -43,8 +75,7 @@ public class ConnFactory {
 				try {
 					resource.close();
 				} catch (Exception e) {
-					System.out.println("Erro ao tentar liberar recurso: " + resource);
-					e.printStackTrace();
+					throw new RuntimeException("Erro ao tentar liberar recurso: " + resource + "\n" + e);
 				}
 			}
 		}
